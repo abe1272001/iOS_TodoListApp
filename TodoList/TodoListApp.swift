4e098df3f0822f66dbf5adcd1fa2027303f2a7a1
abe chen <abe.chen@farmland.tw>
2022-07-14 10:03:24 +0800

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by abe chen on 2022/7/14.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
